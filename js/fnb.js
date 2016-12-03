32d3b798f0df7e8a993b93b71bac019b227ec329
Ayo <helloayo@users.noreply.github.com>
2016-12-03 01:03:59 -0800

    inouts catchall...getting this to work with speech inptu

--- js/fnb.js
+++ js/fnb.js
@@ -29,7 +29,7 @@
 				for (var i = 0; i < choices.length; i++) {
 					$option = $('<input name="' + index + '" id="' + index + '_' + i + '" class="speech-input" type="text">');
 					$choiceEl.append($option);
-					$option.on('focusin',  function() {
+					$option.on('propertychange change click keyup input paste focusin',  function() {
                         
 						return getChosen();
 					});
