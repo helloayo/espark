69c72b65fdfe2dbcd2b0678d3e10da9e10c7a8f4
Ayo <helloayo@users.noreply.github.com>
2016-12-03 09:16:20 -0800

    inputs fix

--- js/fnb.js
+++ js/fnb.js
@@ -29,7 +29,7 @@
 				for (var i = 0; i < choices.length; i++) {
 					$option = $('<input name="' + index + '" id="' + index + '_' + i + '" class="speech-input" type="text">');
 					$choiceEl.append($option);
-					$option.on('input paste focusin',  function() {
+					$option.on('input paste focusin', function() {
                         
 						return getChosen();
 					});
